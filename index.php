07bcc0529a52e7def52714c40dfd3c28db29b62e
Jose <josse180804@gmail.com>
2025-03-17 12:59:44 +0100

    Pages

--- index.php
+++ index.php
@@ -233,28 +233,28 @@
                 <p>━━━━━━━</p>
                 <p class="text-xs mb-2">Síguenos en nuestras redes sociales para estar al día de todas nuestras novedades y eventos.</p>
                 <!--Social buttons-->
-                <button type="button" class="text-white w-full sm:w-2/3 bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-xs px-4 py-2 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 me-2 mb-1">
+                <button type="button" class="text-white w-3/4 sm:w-2/3 bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-xs px-4 py-2 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 me-2 mb-1">
                     <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
                         <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd"/>
                     </svg>
                     Sign in with Facebook
                 </button>
                 <!--Twitter-->
-                <button type="button" class="text-white w-full sm:w-2/3 bg-[#1da1f2] hover:bg-[#1da1f2]/90 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg text-xs px-4 py-2 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55 me-2 mb-1">
+                <button type="button" class="text-white w-3/4 sm:w-2/3 bg-[#1da1f2] hover:bg-[#1da1f2]/90 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg text-xs px-4 py-2 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55 me-2 mb-1">
                     <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 17">
                         <path fill-rule="evenodd" d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z" clip-rule="evenodd"/>
                     </svg>
                     Sign in with Twitter
                 </button>
                 <!--Google-->
-                <button type="button" class="text-white w-full sm:w-2/3 bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-xs px-4 py-2 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-1">
+                <button type="button" class="text-white w-3/4 sm:w-2/3 bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-xs px-4 py-2 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-1">
                     <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
                         <path fill-rule="evenodd" d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z" clip-rule="evenodd"/>
                     </svg>
                     Sign in with Google
                 </button>
                 <!--WhatsApp-->
-                <button type="button" class="text-white w-full sm:w-2/3 bg-[#25D366] hover:bg-[#25D366]/90 focus:ring-4 focus:outline-none focus:ring-[#25D366]/50 font-medium rounded-lg text-xs px-4 py-2 text-center inline-flex items-center dark:focus:ring-[#25D366]/55 me-2 mb-1">
+                <button type="button" class="text-white w-3/4 sm:w-2/3 bg-[#25D366] hover:bg-[#25D366]/90 focus:ring-4 focus:outline-none focus:ring-[#25D366]/50 font-medium rounded-lg text-xs px-4 py-2 text-center inline-flex items-center dark:focus:ring-[#25D366]/55 me-2 mb-1">
                     <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                         <path fill-rule="evenodd" d="M20.463 3.488C18.217 1.24 15.231 0 12.05 0 5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.304-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893 0-3.181-1.237-6.167-3.479-8.413zM12.05 21.785h-.004a9.867 9.867 0 01-5.031-1.378l-.36-.214-3.741.981 1-3.648-.235-.374A9.844 9.844 0 012.157 11.892c0-5.46 4.444-9.902 9.897-9.902 2.641 0 5.123 1.03 6.988 2.898a9.837 9.837 0 012.898 6.994c-.003 5.46-4.447 9.903-9.89 9.903zm5.425-7.417c-.299-.149-1.764-.87-2.037-.969-.274-.099-.473-.149-.672.149-.199.298-.771.969-.945 1.168-.174.199-.348.223-.647.074-.3-.149-1.267-.467-2.413-1.488-.892-.796-1.494-1.777-1.669-2.076-.174-.299-.018-.46.131-.609.135-.134.299-.348.448-.522.149-.174.199-.298.298-.497.099-.199.05-.373-.025-.522-.075-.149-.672-1.619-.921-2.217-.242-.581-.487-.502-.672-.511-.174-.008-.373-.01-.572-.01-.199 0-.522.074-.796.373-.274.299-1.045 1.019-1.045 2.488 0 1.469 1.07 2.889 1.219 3.088.149.199 2.096 3.201 5.077 4.487.709.306 1.263.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.273-.198-.572-.347z" clip-rule="evenodd"/>
                     </svg>
---
+++ pages/encuentranos.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bioespacio - Centro de bienestar natural</title>
+    <link href="../node_modules/flowbite/dist/flowbite.min.css" rel="stylesheet" alt="Estilos de Flowbite">
+    <link href="../assets/css/src/output.css" rel="stylesheet" alt="Estilos personalizados">
+</head>
+<body>
+    <!-- Header -->
+    <?php require_once '../includes/header.php'; ?>
+
+    <main>
+        <div class="bg-beige grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <div class="bg-menta">
+
+        </div>
+    </main>
+    <!-- Footer -->
+    <?php require_once '../includes/footer.php'; ?>
+</body>
+    <!-- Scripts al final del body -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="node_modules/flowbite/dist/flowbite.min.js"></script>
+    <script src="assets/js/script.js" alt="Script principal"></script>
+</html>
