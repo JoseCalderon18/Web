404f7af2a47f226955f19a402b5d2a58984b73cc
Migue-OP <miguel.ortegapalacios@gmail.com>
2025-04-23 09:08:18 +0200

    Mejorar la accesibilidad
    Se ha mejorado la accesibilidad en las páginas de Nosotras y Equipo.

--- pages/equipo.php
+++ pages/equipo.php
@@ -17,34 +17,35 @@
                 <!-- Miembro 1 -->
                 <div class="flex flex-col items-center">
                     <img src="../assets/img/persona1.jpg" alt="Persona 1"
-                        class="w-56 h-56 object-cover rounded-full border-[8px] border-gray-500 hover:scale-105 duration-300"
+                        class="w-56 h-56 object-cover rounded-full border-[8px] border-gray-700 hover:scale-105 duration-300"
                         data-modal-target="modal1" data-modal-toggle="modal1">
-                    <h2 class="mt-4 text-xl font-semibold text-gray-500">Nombre 1</h2>
+                    <h2 class="mt-4 text-xl font-semibold text-gray-800">Nombre 1</h2>
                 </div>
                 <!-- Miembro 2 -->
                 <div class="flex flex-col items-center">
                     <img src="../assets/img/persona2.jpg" alt="Persona 2"
-                        class="w-56 h-56 object-cover rounded-full border-[8px] border-gray-500 hover:scale-105 duration-300"
+                        class="w-56 h-56 object-cover rounded-full border-[8px] border-gray-700 hover:scale-105 duration-300"
                         data-modal-target="modal2" data-modal-toggle="modal2">
-                    <h2 class="mt-4 text-xl font-semibold text-gray-500">Nombre 2</h2>
+                    <h2 class="mt-4 text-xl font-semibold text-gray-800">Nombre 2</h2>
                 </div>
                 <!-- Miembro 3 -->
                 <div class="flex flex-col items-center">
                     <img src="../assets/img/persona3.jpg" alt="Persona 3"
-                        class="w-56 h-56 object-cover rounded-full border-[8px] border-gray-500 hover:scale-105 duration-300"
+                        class="w-56 h-56 object-cover rounded-full border-[8px] border-gray-700 hover:scale-105 duration-300"
                         data-modal-target="modal3" data-modal-toggle="modal3">
-                    <h2 class="mt-4 text-xl font-semibold text-gray-500">Nombre 3</h2>
+                    <h2 class="mt-4 text-xl font-semibold text-gray-800">Nombre 3</h2>
                 </div>
                 <!-- Miembro 4 -->
                 <div class="flex flex-col items-center">
                     <img src="../assets/img/persona4.jpg" alt="Persona 4"
-                        class="w-56 h-56 object-cover rounded-full border-[8px] border-gray-500 hover:scale-105 duration-300"
+                        class="w-56 h-56 object-cover rounded-full border-[8px] border-gray-700 hover:scale-105 duration-300"
                         data-modal-target="modal4" data-modal-toggle="modal4">
-                    <h2 class="mt-4 text-xl font-semibold text-gray-500">Nombre 4</h2>
+                    <h2 class="mt-4 text-xl font-semibold text-gray-800">Nombre 4</h2>
                 </div>
             </div>
         </div>
     </main>
+
 
     <?php require_once '../includes/footer.php'; ?>
 
