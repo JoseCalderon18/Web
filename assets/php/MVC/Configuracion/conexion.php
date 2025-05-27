e2e10ab4cdbd38939fdc8bf05e5c941c6ff93e42
Migue-OP <miguel.ortegapalacios@gmail.com>
2025-05-27 19:30:29 +0200

    Conexión
    Cambios en conexión para que funcione con la base de datos de ionos.

--- assets/php/MVC/Configuracion/conexion.php
+++ assets/php/MVC/Configuracion/conexion.php
@@ -1,23 +1,22 @@
 <?php
-// Clase para la conexion a la base de datos
+// Clase para la conexión a la base de datos en IONOS
 class Conexion{
-    private static $host = "localhost";
-    private static $usuario = "root";
-    private static $contrasenia = "";
-    private static $base_de_datos = "bioespacio";
+    private static $host = "db5017922334.hosting-data.io";
+    private static $usuario = "dbu1561636";
+    private static $contrasenia = "Bioespacio11@";
+    private static $base_de_datos = "dbs14267429";
 
     public static function conectar(){
         try{
-            // Conexion a la base de datos
-            $conexion = new PDO("mysql:host=". self::$host . ";dbname=" . self::$base_de_datos, self::$usuario, self::$contrasenia);
+            // Conexión a la base de datos
+            $conexion = new PDO("mysql:host=" . self::$host . ";port=3306;dbname=" . self::$base_de_datos, self::$usuario, self::$contrasenia);
             $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $conexion->exec("SET NAMES 'utf8'");
             
-            // Retorna la conexion
+            // Retorna la conexión
             return $conexion;
-            
-        }catch(PDOException $e){
-            die("Error de conexion: " . $e->getMessage());
+        } catch(PDOException $e){
+            die("Error de conexión: " . $e->getMessage());
         }
     }
 }
